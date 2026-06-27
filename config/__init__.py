e904b9bf32585ba18a9f2eb0504db283fa50bd11
Mohammed-Magdy-19 <mm0036101@gmail.com>
2026-06-28 00:48:21 +0300

    Commiting Celery app config

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,6 @@
 # This file makes `config` a Python package.
 # Import Celery app here so Django's auto-discovery picks it up.
 
-from .celery import app as celery_app
+# from .celery import app as celery_app
 
-__all__ = ("celery_app",)
+# __all__ = ("celery_app",)
