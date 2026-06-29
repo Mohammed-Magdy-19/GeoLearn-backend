8d93ce7df348963dca0fba91281b344eb385b980
Mohammed-Magdy-19 <mm0036101@gmail.com>
2026-06-29 14:18:42 +0300

    feat: initialize application configuration settings module

--- config/settings.py
+++ config/settings.py
@@ -303,7 +303,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = config("MEDIA_ROOT", default=str(BASE_DIR / "media"))
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
